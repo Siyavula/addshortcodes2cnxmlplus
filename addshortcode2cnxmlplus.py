a91ae5080a45f99963136c4f82ef9ae08c1cf266
Ewald Zietsman <ewald.zietsman@gmail.com>
2012-11-06 23:16:24 +0200

    fixed bug, added function to deal with adding and changing shortcode element to parent element

--- addshortcode2cnxmlplus.py
+++ addshortcode2cnxmlplus.py
@@ -9,54 +9,72 @@ import lxml.etree as etree
 
 import dencoder
 
-#     /document/content//exercises//problem-set/entry
-#     /document/content//presentation
-#     /document/content//simulation
-#     /document/content//video
+#   /document/content//presentation
+#   /document/content//simulation
+#   /document/content//video
+#   /document/content//exercises//problem-set/entry
+#   /document/content//exercises//multi-part
+
+def addShortcodeTag(element, shortcode):
+    '''checks if element has shortcode tag as a child, replaces the contents
+    with the given shortcode or creates a new shortcode element and returns the
+    given element'''
+    # Check if there is a shortcode tag, and if not add one
+    shortcodetag = element.find('shortcode')
+    if shortcodetag is None:
+        newShortcode = etree.Element('shortcode')
+        newShortcode.text = shortcode
+        newShortcode.tail = '\n'
+        element.insert(0, newShortcode)
+    else:
+        shortcodetag.text = shortcode
+
+    return element
 
 if __name__ == "__main__":
     args = sys.argv
 
     if len(args) != 5:
-        print '''Usage: %s cnxmlplusfile.cnxmlplus SectionPrefix SectionStartValue ExStartValue
+        print '''Usage: %s cnxmlplusfile.cnxmlplus SectionPrefix SectionStart ExStart
         
 Adds shortcodes to sections and problem sets in cnxmlplus file. Writes to standard output
 
 SectionPrefix: String that gets appended to shortcode
-SectionStartValue: integer number where first section shortcode starts
-ExStartValue: integer number where first solution shortcode starts''' %args[0]
+SectionStart: starting shortcode for sections
+ExStart: starting shortcode for problems''' %args[0]
         sys.exit(1)
-
-    SectionPrefix = sys.argv[2]
-    SectionStartValue = int(sys.argv[3])
-    ExStartValue = int(sys.argv[4])
-
-    xmlfile = etree.parse(sys.argv[1])
-    root = xmlfile.getroot()
-
 
     # Encoder
     Dencoder = dencoder.Dencoder(alphabet=dencoder.Dencoder.SIYAVULA_ALPHABET)
     
+    SectionPrefix = sys.argv[2]
+    SectionStart = Dencoder.decode(sys.argv[3])
+    ExStart = Dencoder.decode(sys.argv[4])
+
+    xmlfile = etree.parse(sys.argv[1])
+    root = xmlfile.getroot()
+
     for element in root.iter():
-        path = r'/'.join(reversed([a.tag for a in element.iterancestors()]))  #TODO bug, add tag name
+        path = r'/'.join(reversed([a.tag for a in element.iterancestors()])) +\
+        r'/' + str(element.tag)
         
         # Add section shortcodes
         if element.tag == 'section':
-            if (path == r'document/content/section') or (path == r'document/content/section/section'):
+            if (path == r'document/content/section/section') or (path ==
+                    r'document/content/section/section/section'):
                 # These are sections and subsections
+                newshortcode = SectionPrefix + Dencoder.encode(SectionStart)
+                element = addShortcodeTag(element, newshortcode) 
+                SectionStart += 1
 
-                # Check if there is a shortcode tag, and if not add one
-                shortcodetag = element.find('shortcode')
-                if shortcodetag is None:
-                    newShortcode = etree.Element('shortcode')
-                    newShortcode.text = SectionPrefix + Dencoder.encode(SectionStartValue)
-                    newShortcode.tail = '\n'
-                    element.insert(0, newShortcode)
-                else:
-                    shortcodetag.text = SectionPrefix + Dencoder.encode(SectionStartValue)
+        if element.tag in ['presentation', 'simulation', 'video']:
+            # maybe create better shortcode for simulations, presentations and
+            # videos
+            newshortcode = 'V' + Dencoder.encode(ExStart)
+            element = addShortcodeTag(element, newshortcode)
+            ExStart += 1
 
-                SectionStartValue += 1
+
 
 
 
